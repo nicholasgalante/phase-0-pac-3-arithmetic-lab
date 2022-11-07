2824ab4f10d5e38013a0eb3ee74b70709c17be02
Nicholas <nicholasgalante94@gmail.com>
2022-11-06 21:27:48 -0500

    commit tree b9945a71e6d3dba1193239611bbd17232849dd62

---
+++ index.js
@@ -0,0 +1,9 @@
+add = (a, b) => a + b;
+subtract = (a, b) => a - b;
+multiply = (a, b) => a * b;
+divide = (a, b) => a / b;
+increment = (n) => n+1;
+decrement = (n) => n-1;
+makeInt = (n) => parseInt(n, 10);
+preserveDecimal = (n) => parseFloat(n);
+
